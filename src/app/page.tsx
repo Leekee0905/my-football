5fb3a00c843c2f72ad4f50fded8c731ba74cef70
GiSeong Lee <cj8928@gmail.com>
2025-02-13 21:05:18 +0900

    add: 페이지 파일 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-const Home = () => {
-  return <div></div>;
+import LeagueTable from "@/components/LeaugeTable";
+
+const Home = async () => {
+  return (
+    <div className="grid grid-cols-[4fr_6fr] gap-4 px-[152px]">
+      <div className="border-black border-solid border-2">
+        <LeagueTable />
+      </div>
+      <div className="border-blue-500 border-solid border-2"></div>
+    </div>
+  );
 };
 
 export default Home;
