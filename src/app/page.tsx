d2f281c4cefe682ac50c9db68e808efc4cfa288b
GiSeong Lee <cj8928@gmail.com>
2025-02-15 02:06:31 +0900

    delete: 홈 테이블 border 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import LeagueTable from "@/components/LeaugeTable";
 
 const Home = async () => {
   return (
-    <div className="grid grid-cols-[4fr_6fr] gap-4 px-[152px]">
-      <div className="border-black border-solid border-2">
+    <div className="grid grid-cols-[4fr_6fr] gap-4">
+      <div>
         <LeagueTable />
       </div>
       <div className="border-blue-500 border-solid border-2"></div>
