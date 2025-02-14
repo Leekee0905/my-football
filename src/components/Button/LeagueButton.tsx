02950ccaeb2ae00a14514bc8c6e2927f33f7e88f
GiSeong Lee <cj8928@gmail.com>
2025-02-15 02:10:04 +0900

    fix: 리그 키값 변경

--- src/components/Button/LeagueButton.tsx
+++ src/components/Button/LeagueButton.tsx
@@ -5,17 +5,22 @@ import { LEAGUE } from "@/constants/leagues";
 type LeagueButtonProps = {
   isActive: boolean;
   onClick: (label: string) => void;
-  label: string;
+  leagueKey: string;
 };
 
-const LeagueButton = ({ isActive, onClick, label }: LeagueButtonProps) => {
-  console.log(label, LEAGUE);
+const LeagueButton = ({ isActive, onClick, leagueKey }: LeagueButtonProps) => {
   return (
     <button
-      className={isActive ? "bg-purple-600" : "bg-white"}
-      onClick={() => onClick(label)}
+      className={`rounded-md flex-1 min-w-max bg-white ${
+        isActive ? "bg-purple-800" : "bg-white"
+      }`}
+      onClick={() => onClick(leagueKey)}
     >
-      <p className={isActive ? "text-white" : "text-black"}>{label}</p>
+      <p
+        className={`px-4 text-[14px] ${isActive ? "text-white" : "text-black"}`}
+      >
+        {LEAGUE[leagueKey]}
+      </p>
     </button>
   );
 };
--- src/components/Button/LeagueButtonContainer.tsx
+++ src/components/Button/LeagueButtonContainer.tsx
@@ -11,18 +11,20 @@ const LeagueButtonContainer = ({
   onClick,
 }: LeagueButtonContainerProps) => {
   return (
-    <div className="flex justify-center items-center gap-4">
-      {Object.entries(LEAGUE).map(([leagueKey, leagueName]) => {
-        const isActive = currentTableLeagueName === leagueName;
-        return (
-          <LeagueButton
-            key={leagueKey}
-            isActive={isActive}
-            onClick={onClick}
-            label={leagueName}
-          />
-        );
-      })}
+    <div className="flex justify-center items-center">
+      <div className="flex w-full max-w-max gap-4">
+        {Object.keys(LEAGUE).map((leagueKey) => {
+          const isActive = currentTableLeagueName === leagueKey;
+          return (
+            <LeagueButton
+              key={leagueKey}
+              isActive={isActive}
+              onClick={onClick}
+              leagueKey={leagueKey}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
