0de4142af14cbc6ca71ad8a85f033db8b6bd12ca
LGS <cj8928@gmail.com>
2025-02-17 17:19:11 +0900

    add: 에러 페이지 생성

---
+++ src/app/error.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { useEffect } from "react";
+
+const Error = ({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) => {
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+
+  return <div>error</div>;
+};
+export default Error;
