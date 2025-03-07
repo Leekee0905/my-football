c406a7165756a6f22b705b3b8fcc16048567ba90
GiSeong Lee <cj8928@gmail.com>
2025-03-07 17:51:06 +0900

    update: 테이블 페이지 업데이트

--- src/app/table/page.tsx
+++ src/app/table/page.tsx
@@ -1,8 +1,12 @@
-import LeagueTable from "@/components/LeaugeTable";
+import LeagueButtonContainer from "@/components/button/LeagueButtonContainer";
+import SeasonNavigationButton from "@/components/button/SeasonNavigationButton";
+import LeagueTable from "@/components/table/LeaugeTable";
 
 const Table = () => {
   return (
-    <div>
+    <div className="max-w-[1200px] flex flex-col justify-center items-center gap-4">
+      <SeasonNavigationButton />
+      <LeagueButtonContainer />
       <LeagueTable />
     </div>
   );
