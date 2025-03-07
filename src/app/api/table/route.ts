0f24096905040493a14f5da53019af774ac945c5
GiSeong Lee <cj8928@gmail.com>
2025-03-07 17:50:32 +0900

    update: url 템플릿 문자열에서 new URL방식으로 변경

--- src/app/api/table/route.ts
+++ src/app/api/table/route.ts
@@ -3,15 +3,24 @@ import { NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
-  const query = searchParams.get("query");
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/competitions/${query}/standings`,
-    {
-      headers: {
-        "X-Auth-Token": process.env.NEXT_PUBLIC_FOOTBALL_API_KEY as string,
-      },
-    }
+  const league = searchParams.get("league");
+  const season = searchParams.get("season");
+  if (!season)
+    return Response.json(
+      { error: "Season parameter is required" },
+      { status: 400 }
+    );
+
+  const apiUrl = new URL(
+    `${process.env.NEXT_PUBLIC_API_URL}/competitions/${league}/standings`
   );
+  apiUrl.searchParams.append("season", season);
+
+  const res = await fetch(apiUrl, {
+    headers: {
+      "X-Auth-Token": process.env.NEXT_PUBLIC_FOOTBALL_API_KEY as string,
+    },
+  });
   const data: PremierLeagueData = await res.json();
 
   return Response.json(data);
