3b25083b98f38b6d147a0cda8ae61c99ae414243
GiSeong Lee <cj8928@gmail.com>
2025-03-07 18:04:34 +0900

    update: 리그 테이블 가져오는 함수 시즌 업데이트

--- src/fetch/getLeagueTable.ts
+++ src/fetch/getLeagueTable.ts
@@ -1,7 +1,7 @@
 import { PremierLeagueData } from "@/types/tableDataType.type";
 
-const getLeagueTable = async (league: string) => {
-  const url = `/api/table?query=${league}`;
+const getLeagueTable = async (league: string, season: number) => {
+  const url = `/api/table?league=${league}&season=${season}`;
   const res = await fetch(url);
   const data: PremierLeagueData = await res.json();
   return data;
