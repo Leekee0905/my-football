dae0bc6eea2cee546618282fd11ede64f3764c0f
GiSeong Lee <cj8928@gmail.com>
2025-03-07 18:25:35 +0900

    delete: store season 삭제

--- src/store/leagueStore.ts
+++ src/store/leagueStore.ts
@@ -2,22 +2,16 @@ import { createStore } from "zustand";
 
 export interface LeagueState {
   league: string;
-  season: number;
 }
 
 export interface LeagueActions {
   setLeague: (leagueName: string) => void;
-  setSeason: (season: number) => void;
 }
 
 export type LeagueStore = LeagueState & LeagueActions;
 
 export const defaultInitialState: LeagueState = {
   league: "PL",
-  season:
-    new Date().getMonth() + 1 > 7
-      ? new Date().getFullYear()
-      : new Date().getFullYear() - 1,
 };
 
 export const createLeagueStore = (
@@ -26,6 +20,5 @@ export const createLeagueStore = (
   return createStore<LeagueStore>()((set) => ({
     ...initState,
     setLeague: (leagueName) => set({ league: leagueName }),
-    setSeason: (season) => set({ season: season }),
   }));
 };
