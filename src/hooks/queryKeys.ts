e2349a13fbf3009130d379025a481d33299faba1
GiSeong Lee <cj8928@gmail.com>
2025-03-07 17:57:49 +0900

    update: query key season 업데이트

--- src/hooks/queryKeys.ts
+++ src/hooks/queryKeys.ts
@@ -1,3 +1,6 @@
 export const QUERY_KEYS = {
-  leagueTable: (league: string) => ["leagueTable", league] as const,
+  leagueTable: (league: string, season: number) =>
+    ["leagueTable", league, season] as const,
+  weeklyLeagueSchedule: (league: string) =>
+    ["weeklyLeagueSchedule", league] as const,
 };
