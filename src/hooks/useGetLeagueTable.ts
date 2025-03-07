c6b7fee6999f82eab7c2ba21e77d8fabb17eaedf
GiSeong Lee <cj8928@gmail.com>
2025-03-07 17:52:49 +0900

    update: 시즌 추가

--- src/hooks/useGetLeagueTable.ts
+++ src/hooks/useGetLeagueTable.ts
@@ -2,10 +2,10 @@ import getLeagueTable from "@/fetch/getLeagueTable";
 import { useQuery } from "@tanstack/react-query";
 import { QUERY_KEYS } from "./queryKeys";
 
-const useGetLeagueTableQuery = (league: string) => {
+const useGetLeagueTableQuery = (league: string, season: number) => {
   return useQuery({
-    queryKey: QUERY_KEYS.leagueTable(league),
-    queryFn: () => getLeagueTable(league),
+    queryKey: QUERY_KEYS.leagueTable(league, season),
+    queryFn: () => getLeagueTable(league, season),
   });
 };
 
