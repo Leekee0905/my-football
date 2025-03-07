cf8115c4c6b53a155bab1461a24a223ce174bad9
GiSeong Lee <cj8928@gmail.com>
2025-03-07 18:05:38 +0900

    update: 헤더 스타일 변경

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -4,9 +4,11 @@ import NavigationLink from "./NavigationLink";
 
 const Header = () => {
   return (
-    <header className="flex w-full bg-gray-100">
-      <div className="w-[1440px] mx-auto flex h-[86px] justify-between items-center px-[120px] bg-white shadow-[0_4px_20px_rgba(0,0,0,0.1)]">
-        <Link href="/">MyFootBall</Link>
+    <header className="flex w-full">
+      <div className="max-w-[1440px] w-full mx-auto flex h-[86px] justify-between items-center px-28 bg-white">
+        <h1>
+          <Link href="/">MyFootBall</Link>
+        </h1>
         <nav className="flex gap-[24px] justify-center items-center">
           {Object.entries(NAVIGATION_LINKS).map(([link, name]) => (
             <NavigationLink key={link} path={link} name={name} />
