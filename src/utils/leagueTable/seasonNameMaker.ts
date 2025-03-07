d55bc5bafe8abe66f4b8f0ccee4da109695a9db8
GiSeong Lee <cj8928@gmail.com>
2025-03-07 17:47:57 +0900

    delete: 미사용 함수 삭제

--- src/utils/leagueTable/seasonNameMaker.ts
+++
@@ -1,8 +0,0 @@
-const seasonNameMaker = (season: string | undefined) => {
-  if (season === undefined) {
-    return "";
-  }
-  return `${season} - ${Number(season) + 1}시즌`;
-};
-
-export default seasonNameMaker;
