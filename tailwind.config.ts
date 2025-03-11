4cf5e493ff0722e11ac5dd039276dfa784fce4aa
GiSeong Lee <cj8928@gmail.com>
2025-03-11 18:08:30 +0900

    script: tailwindcss scrollbar hide 플러그인 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-
+import scrollbarHide from "tailwind-scrollbar-hide";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -18,5 +18,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [scrollbarHide],
 } satisfies Config;
