e8959b9cbac4714543423f591327e9485a3a217f
GiSeong Lee <cj8928@gmail.com>
2025-03-12 18:31:54 +0900

    add: 일정 스켈레톤 추가

---
+++ src/components/schedule/SkeletonSchedule.tsx
@@ -0,0 +1,7 @@
+const SkeletonSchedule = () => {
+  return (
+    <div className="animate-pulse bg-gray-200 border-gray-200 border-solid border rounded-lg mt-[30px] w-full h-20"></div>
+  );
+};
+
+export default SkeletonSchedule;
