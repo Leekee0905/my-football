309e2fda57be7cabbe0bf97c515d7fda4539ccba
GiSeong Lee <cj8928@gmail.com>
2025-03-12 18:29:01 +0900

    add: 상태에 따른 색상 컨버터

--- src/utils/schedule/statusConverter.ts
+++ src/utils/schedule/statusConverter.ts
@@ -20,4 +20,23 @@ const statusConverter = (status: string) => {
   }
 };
 
+export const statusColorConverter = (status: string) => {
+  switch (status) {
+    case "FINISHED":
+      return "text-gray-500";
+    case "TIMED":
+    case "SCHEDULED":
+      return "text-blue-500";
+    case "LIVE":
+      return "text-green-500";
+    case "PAUSED":
+    case "POSTPONED":
+    case "SUSPENDED":
+    case "CANCELLED":
+      return "text-red-500";
+    default:
+      break;
+  }
+};
+
 export default statusConverter;
