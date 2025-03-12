85b5dc97db29129d6574375a7ee421cfeb745ffc
GiSeong Lee <cj8928@gmail.com>
2025-03-12 18:28:34 +0900

    add: 팀 가져오는 route handler

---
+++ src/app/api/schedule/teams/route.ts
@@ -0,0 +1,27 @@
+import { TeamsDataType } from "@/types/teamsDataType.type";
+import { NextRequest } from "next/server";
+
+export const GET = async (request: NextRequest) => {
+  const searchParams = request.nextUrl.searchParams;
+  const league = searchParams.get("league");
+  const season = searchParams.get("season");
+  if (!season)
+    return Response.json(
+      { error: "Season parameter is required" },
+      { status: 400 }
+    );
+
+  const apiUrl = new URL(
+    `${process.env.NEXT_PUBLIC_API_URL}/competitions/${league}/teams`
+  );
+  apiUrl.searchParams.append("season", season);
+
+  const res = await fetch(apiUrl, {
+    headers: {
+      "X-Auth-Token": process.env.NEXT_PUBLIC_FOOTBALL_API_KEY as string,
+    },
+  });
+  const data: TeamsDataType = await res.json();
+
+  return Response.json(data);
+};
