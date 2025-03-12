e54a70a359179c3210ff2172066ec082de58d3e4
GiSeong Lee <cj8928@gmail.com>
2025-03-12 18:34:30 +0900

    fix: 테이블 데이터 타입 수정

--- src/fetch/getLeagueTable.ts
+++ src/fetch/getLeagueTable.ts
@@ -1,9 +1,9 @@
-import { PremierLeagueData } from "@/types/tableDataType.type";
+import { LeagueDataType } from "@/types/tableDataType.type";
 
 const getLeagueTable = async (league: string, season: number) => {
-  const url = `/api/table?league=${league}&season=${season}`;
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/table?league=${league}&season=${season}`;
   const res = await fetch(url);
-  const data: PremierLeagueData = await res.json();
+  const data: LeagueDataType = await res.json();
   return data;
 };
 
