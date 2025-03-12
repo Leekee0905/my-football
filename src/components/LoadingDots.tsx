13259d5255ea687cf17b9fb4fc062c6785ead5e8
GiSeong Lee <cj8928@gmail.com>
2025-03-12 18:29:47 +0900

    update: 로딩 UI 색상 변경

--- src/components/LoadingDots.tsx
+++ src/components/LoadingDots.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const LoadingDots = () => {
   return (
     <div className="flex justify-center items-center space-x-2">
-      <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce [animation-delay:0s]"></span>
-      <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce [animation-delay:0.2s]"></span>
-      <span className="w-3 h-3 bg-gray-500 rounded-full animate-bounce [animation-delay:0.4s]"></span>
+      <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:0s]"></span>
+      <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:0.2s]"></span>
+      <span className="w-3 h-3 bg-blue-500 rounded-full animate-bounce [animation-delay:0.4s]"></span>
     </div>
   );
 };
