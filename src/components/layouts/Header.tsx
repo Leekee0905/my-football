c2f8f54644ea06f08222a3a2cd42a0415bc1be93
GiSeong Lee <cj8928@gmail.com>
2025-03-12 18:29:33 +0900

    update: header 로고

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,13 +1,21 @@
 import { NAVIGATION_LINKS } from "@/constants/navigation";
 import Link from "next/link";
 import NavigationLink from "./NavigationLink";
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header className="flex w-full">
       <div className="max-w-[1440px] w-full mx-auto flex h-[86px] justify-between items-center px-28 bg-white">
         <h1>
-          <Link href="/">MyFootBall</Link>
+          <Link href="/">
+            <Image
+              src={"/assets/images/logo/logo.png"}
+              alt="로고"
+              width={100}
+              height={100}
+            />
+          </Link>
         </h1>
         <nav className="flex gap-[24px] justify-center items-center">
           {Object.entries(NAVIGATION_LINKS).map(([link, name]) => (
