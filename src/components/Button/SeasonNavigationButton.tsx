00cca41498b9c6bdbe55e29e1afca3c5745a4f9c
GiSeong Lee <cj8928@gmail.com>
2025-03-12 18:30:16 +0900

    update: 벡터이미지 컴포넌트화

--- src/components/Button/SeasonNavigationButton.tsx
+++ src/components/Button/SeasonNavigationButton.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import getThisSeason from "@/utils/getThisSeason";
+import { LeftArrow, RightArrow } from "../Arrow";
 
 type SeasonNavigationButtonProps = {
   season: number;
@@ -18,49 +19,23 @@ const SeasonNavigationButton = ({
   return (
     <div className="flex justify-center items-center space-x-4">
       <button
+        aria-label="prev"
         onClick={handlePrev}
         className="px-4 py-2 text-black text-xl rounded-lg"
         disabled={isPreviousDisabled}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="8"
-          height="14"
-          fill="none"
-          aria-hidden="true"
-          className={isPreviousDisabled ? "stroke-gray-400" : "stroke-black"}
-        >
-          <path
-            d="M7 13L1 6.984 7 1"
-            strokeWidth="1.6"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-        </svg>
+        <LeftArrow isDisabled={isPreviousDisabled} />
       </button>
       <span className="text-2xl font-semibold">
         {season}-{season + 1}
       </span>
       <button
+        aria-label="next"
         onClick={handleNext}
         className="px-4 py-2  rounded-lg"
         disabled={isNextDisabled}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="8"
-          height="14"
-          fill="none"
-          aria-hidden="true"
-          className={isNextDisabled ? "stroke-gray-400" : "stroke-black"}
-        >
-          <path
-            d="M1 13L7 6.984 1 1"
-            strokeWidth="1.6"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          ></path>
-        </svg>
+        <RightArrow isDisabled={isNextDisabled} />
       </button>
     </div>
   );
