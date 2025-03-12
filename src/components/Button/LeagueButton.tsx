529f650e759d9131e4cb9e761ce386157f9e2e66
GiSeong Lee <cj8928@gmail.com>
2025-03-12 18:30:30 +0900

    fix: border 수정

--- src/components/Button/LeagueButton.tsx
+++ src/components/Button/LeagueButton.tsx
@@ -11,14 +11,14 @@ const LeagueButton = ({ isActive, leagueKey }: LeagueButtonProps) => {
   const handleLeagueButton = useLeagueStore((state) => state.setLeague);
   return (
     <button
-      className={`rounded-full flex-1 min-w-max h-[40px] border-2 ${
-        isActive ? "bg-[#4071e3] border-0" : "bg-white"
+      className={`rounded-full flex-1 min-w-max h-[40px] ${
+        isActive ? "bg-[#4071e3]" : "bg-white border-2"
       }`}
       onClick={() => handleLeagueButton(leagueKey)}
     >
       <p
-        className={`px-4 text-[15px] ${
-          isActive ? "text-white font-bold" : "text-black"
+        className={`px-4 text-[15px] font-bold ${
+          isActive ? "text-white" : "text-black"
         }`}
       >
         {LEAGUE[leagueKey]}
